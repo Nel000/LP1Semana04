d78b3374e1f72ababfc3cc11828aab568cb01aff
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-15 18:05:06 +0000

    Add Fibonacci method documentation

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -8,5 +8,16 @@ namespace Fibonacci
         {
             
         }
+
+        /// <summary>
+        /// Determines the number in the Fibonacci sequence that corresponds
+        /// to the given number.
+        /// </summary>
+        /// <param name="n">An index for a number in the sequence</param>
+        /// <returns>The number in the 'n' position of the sequence</returns>
+        private static int Fibonacci(int n)
+        {
+    
+        }
     }
 }
