41e958d955c5451b480e6bb5633062ef6c887827
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-15 18:28:43 +0000

    Return number in Fibonacci sequence correspondent to given index

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,7 +6,8 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            
+            int fib = Fibonacci(8);
+            Console.WriteLine(fib);
         }
 
         /// <summary>
@@ -17,7 +18,18 @@ namespace Fibonacci
         /// <returns>The number in the 'n' position of the sequence</returns>
         private static int Fibonacci(int n)
         {
-    
+            // Stores the current number in the sequence
+            int fib;
+
+            if (n <= 2)
+                // Corresponds to index position 1 or 2, which have value of 1
+                fib = 1;
+            else
+                // Execute recursive operation to obtain correct number
+                fib = Fibonacci(n - 2) + Fibonacci(n - 1);
+            
+            // Return number correspondent to given index
+            return fib;
         }
     }
 }
