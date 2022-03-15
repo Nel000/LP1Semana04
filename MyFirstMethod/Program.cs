99b21628a7122a2b559454cbf60bfb9d525101f1
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-15 16:04:47 +0000

    Restore CountTo10 method, update to invoke CountToN with 10

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -6,14 +6,27 @@ namespace MyFirstMethod
     {
         static void Main(string[] args)
         {
+            // Invoke CountTo10 method twice
+            CountTo10();
+            CountTo10();
+
+            // Invoke CountToN method with 2 different values
             CountToN(5);
             CountToN(7);
         }
 
-        private static void CountToN(int n)
+        private static void CountTo10()
         {
+            // Invoke CountToN method with 10 as value
+            CountToN(10);
+        }
+
+        private static void CountToN(int n)
+        {   
+            // Loop until given number is equal to 'i'
             for (int i = 1; i <= n; i++)
             {
+                // Print one number per line
                 Console.WriteLine(i);
             }
         }
