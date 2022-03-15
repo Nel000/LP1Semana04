a0886ba11338b79188d7a06ea79287b06c03cc60
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-15 16:15:19 +0000

    Add XML documentation

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -2,8 +2,16 @@
 
 namespace MyFirstMethod
 {
+    /// <summary>
+    /// This class contains 2 methods that print one number per line, 
+    /// and invokes them both twice.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Execute both methods in the class.
+        /// </summary>
+        /// <param name="args">Arguments given through the console</param>
         static void Main(string[] args)
         {
             // Invoke CountTo10 method twice
@@ -15,12 +23,19 @@ namespace MyFirstMethod
             CountToN(7);
         }
 
+        /// <summary>
+        /// Print one number per line in order until number 10 is reached.
+        /// </summary>
         private static void CountTo10()
         {
             // Invoke CountToN method with 10 as value
             CountToN(10);
         }
 
+        /// <summary>
+        /// Print one number per line in order until given number is reached.
+        /// </summary>
+        /// <param name="n">The given number to count towards</param>
         private static void CountToN(int n)
         {   
             // Loop until given number is equal to 'i'
